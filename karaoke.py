d67af4086140aa74b3688cf9edddb32c911f8b7a
irene.vt <i.vinagre.2016@alumnos.urjc.es>
2016-10-10 21:22:00 +0200

    Ejercicio 6

--- karaoke.py
+++ karaoke.py
@@ -2,49 +2,67 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import json
+import urllib
 from smallsmilhandler import SmallSMILHandler
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-import json
+
+
+class KaraokeLocal(SmallSMILHandler):
+
+    def __init__(self, fichero):
+        parser = make_parser()
+        cHandler = SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(fichero))
+        self.datos = cHandler.guardadoEtiquetas()
+
+    def __str__(self):
+        pass
+        linea = ""
+        for elemento in listaEtiquetas:
+            linea = linea + elemento[0]
+            atributos = elemento[1].valor()
+            for clave, valor in atributos:
+                linea = linea + '\t' + clave + ' = ' + '"' + valor + '"'
+            linea = linea + '\n'
+        print(linea)
+
+    def to_json(self, ficheroSmil, new=''):
+        if new == '':
+            new = ficheroSmil.split('.')[0] + '.json'
+        with open(new, 'w') as ficheroJson:
+            json.dump(self.datos, ficheroJson, sort_keys=True,
+                      indent=4, separators=(' ', ': '))
+
+    def do_local(self):
+        for elemento in listaEtiquetas:
+            atributos = elemento[1]
+            try:
+                url = atributos['src']
+                if url != "cancion.ogg":
+                    nombreArchivo = url[url.rfind("/") + 1:]
+                    data = urllib.request.urlretrieve(url, nombreArchivo)
+                    atributos['src'] = "http://" + data[0]
+            except KeyError as e:
+                pass
 
 
 def abrirFichero():
     try:
-        fichero = open(sys.argv[1], 'r')
+        fichero = sys.argv[1]
         return fichero
     except IndexError:
-        sys.exit("Usar: python3 karaoke.py archivo.smil")
-  
-
-def obtenerFichero(fichero):
-    parser = make_parser()
-    cHandler = SmallSMILHandler()
-    parser.setContentHandler(cHandler)
-    parser.parse(fichero)
-    list = cHandler.guardadoEtiquetas()
-    return listaEtiquetas
+        sys.exit("Usage: python3 karaoke.py file.smil.")
+    except FileNotFoundError:
+        sys.exit("Not file with this name")
 
 
-def imprimirEtiquetas(listaEtiquetas):
-    pass
-    linea = ""
-    for elemento in listaEtiquetas:
-        linea = linea + elemento[0]
-        atributos = elemento[1].valor()
-        for clave, valor in atributos:
-            linea = linea + '\t' + clave + ' = ' + '"' + valor + '"'
-        linea = linea + '\n'
-    print (linea)
-
-def json(listaJson):
-    with open('karaoke.json', 'w') as fichero_json:
-        json.dump(listaJson, fichero_json, sort_keys=True, indent=4, separators=(' ', ': '))
-
-   
 if __name__ == '__main__':
     fichero = abrirFichero()
-    parser = make_parser()
-    cHandler = SmallSMILHandler()
-    parser.setContentHandler(cHandler)
-    parser.parse(fichero)
-    print(cHandler.guardadoEtiquetas())
+    karaoke = KaraokeLocal(fichero)
+    karaoke.to_json(fichero)
+    karaoke.do_local()
+    karaoke.to_json(fichero, 'local.json')
+    print(karaoke)
