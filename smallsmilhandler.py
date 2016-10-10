7c90b2dc14911cf74a72f13493bde4ccc5c90837
irene.vt <i.vinagre.2016@alumnos.urjc.es>
2016-10-10 20:22:56 +0200

    Ejercicio 3 con Pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -6,34 +6,31 @@ from xml.sax.handler import ContentHandler
 
 
 class SmallSMILHandler(ContentHandler):
-    
+
     def __init__(self):
 
         self.listaEtiquetas = []
-        self.dicionarioEtiquetas = {'root-layout': ['width', 'height', 'background-color'],'region': ['id', 'top', 'bottom', 'left', 'right'], 'img': ['src', 'region', 'begin', 'dur'], 'audio': ['src', 'begin', 'dur'], 'textstream': ['src', 'region']}
+        self.dicionarioEtiquetas = {'root-layout': ['width', 'height', 'background-color'], 'region': ['id', 'top', 'bottom', 'left', 'right'], 'img': ['src', 'region', 'begin', 'dur'], 'audio': ['src', 'begin', 'dur'], 'textstream': ['src', 'region']}
 
-    def startElement(self, name, attrs):
-        
-        if name in self.dicionarioEtiquetas:
+    def startElement(self, clave, atributos):
+
+        if clave in self.dicionarioEtiquetas:
             dicionarioAlmacenado = {}
 
-            for item in self.dicionarioEtiquetas[name]:
-                dicionarioAlmacenado[item] = attrs.get(item, "")
+            for valor in self.dicionarioEtiquetas[clave]:
+                dicionarioAlmacenado[valor] = atributos.get(valor, "")
 
-            dicname = {name: dicionarioAlmacenado}
-            self.listaEtiquetas.append(dicname)
+            dicValor = {valor: dicionarioAlmacenado}
+            self.listaEtiquetas.append(dicValor)
 
     def guardadoEtiquetas(self):
-        
+
         return self.listaEtiquetas
 
 if __name__ == "__main__":
-   
+
     parser = make_parser()
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open('karaoke.smil'))
-    print (cHandler.guardadoEtiquetas())
-
-
-
+    print(cHandler.guardadoEtiquetas())
